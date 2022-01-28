f8f56e890e598de98aaa0e98ae91ea59d940215f
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-01-28 21:35:02 +0530

    added

---
+++ pybot/firstapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http import HttpResponse
+# Create your views here.
+from django import *
+
+def home(request):
+	return HttpResponse("<h1>Hello World</h1>");
