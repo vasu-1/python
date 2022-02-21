ba901dd99dd4056370496ccd27adfa752f79850f
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-21 15:45:49 +0530

    modified test.py

--- Notes/test.py
+++ Notes/test.py
@@ -1,27 +1,5 @@
-from abc import ABC, abstractmethod
+# import os
 
-class Com(ABC):
+var1 =  __file__
 
-	@abstractmethod
-	def process(self):
-		pass
-
-class Desktop(Com):
-	pass
-
-
-class Laptop(Com):
-	def process():
-		print("Working")
-
-
-
-# coma = Com()
-# coma.process()
-#we can't make object of abstract class
-
-comb = Laptop()
-# we can do it as because it has atleast one method
-
-# comb = Desktop()
-# We ca't do it as it is an abstract
+print(var1)
